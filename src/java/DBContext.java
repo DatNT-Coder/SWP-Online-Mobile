376a60ea87f2afd02fe06d9c6541088597d2143c
TDat09 <datnthe173461@fpt.edu.vn>
2025-01-13 07:47:06 +0700

    Add git adtribute

--- src/java/DBContext.java
+++ src/java/DBContext.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class DBContext {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/mydb";
+        String url = "jdbc:mysql://localhost:3306/world";
         String user = "root";
-        String password = "1234";
+        String password = "123456";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to the database!");
