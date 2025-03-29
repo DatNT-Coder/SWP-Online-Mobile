05c1a8ec5fcf248ea985b7c80f1a4c0b11e30d94
TDat09 <datnthe173461@fpt.edu.vn>
2025-03-29 20:35:48 +0700

    Update VNPayConfig.java

--- src/java/controller/cart/VNPayConfig.java
+++ src/java/controller/cart/VNPayConfig.java
@@ -65,7 +65,7 @@ public class VNPayConfig {
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
-        cld.add(Calendar.MINUTE, 15);
+        cld.add(Calendar.MINUTE, 1);
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
